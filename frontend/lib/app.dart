69f0294c9f10bab70db60279d4a1f92ba477a48e
RSR41 <qkrtjdfhr777@gmail.com>
2025-06-07 23:13:42 +0900

    Implement basic auth

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,1 +1,14 @@
-  
+import 'package:flutter/material.dart';
+
+class ZDatingApp extends StatelessWidget {
+  const ZDatingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Z Dating App',
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: const Placeholder(),
+    );
+  }
+}
