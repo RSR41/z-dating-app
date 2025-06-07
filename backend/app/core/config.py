71c0bb36be89ce795d88d8e188159d4c59b8e7dd
RSR41 <qkrtjdfhr777@gmail.com>
2025-06-07 23:14:36 +0900

    Implement basic auth

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -10,6 +10,8 @@ class Settings(BaseSettings):
     DEBUG: bool = True
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     # DB
     DATABASE_URL: str
